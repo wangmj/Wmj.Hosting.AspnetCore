db7c1f53664399c470efb84944029921950e2f7a
明杰 王 <wangmingjie20@outlook.com>
2022-08-28 23:16:31 +0800

    mark

---
+++ Wmj.Hosting/MiddleWares/RequestCounterMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Wmj.Hosting.MiddleWares
+{
+    public class RequestCounterMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public RequestCounterMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                _next(context);
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
