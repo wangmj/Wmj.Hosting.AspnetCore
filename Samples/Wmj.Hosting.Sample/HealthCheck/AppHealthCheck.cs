ab8abee2791620e33209ccd72ef6c2e499410250
Mingjie Wang <mingjwa@microsoft.com>
2022-09-23 15:26:25 +0800

    add health check

---
+++ Samples/Wmj.Hosting.Sample/HealthCheck/AppHealthCheck.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Wmj.Hosting.Sample.HealthCheck
+{
+    public class AppHealthCheck : IHealthCheck
+    {
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            await Task.Delay(1000);
+            //example:check sql, etc..,
+            return HealthCheckResult.Healthy();
+        }
+    }
+}
--- Samples/Wmj.Hosting.Sample/Startup.cs
+++ Samples/Wmj.Hosting.Sample/Startup.cs
@@ -1,8 +1,10 @@
-﻿using Wmj.Hosting.AspnetCore;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Wmj.Hosting.AspnetCore;
 using Wmj.Hosting.AspnetCore.Endpoints;
 using Wmj.Hosting.AspnetCore.LifeTimes;
 using Wmj.Hosting.AspnetCore.MiddleWares;
 using Wmj.Hosting.Sample.Filters;
+using Wmj.Hosting.Sample.HealthCheck;
 
 namespace Wmj.Hosting.Sample
 {
@@ -14,11 +16,13 @@ namespace Wmj.Hosting.Sample
             service.AddAuthenticationCore();
             service.AddAuthorizationCore();
             service.AddScoped<ActionValidateFilter>();
+            service.AddHealthChecks().AddCheck<AppHealthCheck>("app");
         }
 
         public static void BuildService(WebApplication app, IWebHostEnvironment env)
         {
             app.UseMiddleware<RequestErrorHandlerMiddleware>();
+            app.UseHealthChecks("/health");
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
