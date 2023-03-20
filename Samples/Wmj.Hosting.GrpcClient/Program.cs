89b8b5ef20954bb17f85c0b33747dee3b5ea8c40
Mingjie Wang <mingjwa@microsoft.com>
2023-03-20 10:16:39 +0800

    add grpc server and client

---
+++ Samples/Wmj.Hosting.GrpcClient/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using Wmj.Hosting.Grpc.Protos;
+
+
+string grpcServer = "http://localhost:5236";
+try
+{
+    var channel = GrpcChannel.ForAddress(grpcServer);
+    var orderClient = new Order.OrderClient(channel);
+    AddOrderReq req = new AddOrderReq();
+    req.Name = "tttt";
+    req.Num = 1;
+    var addOrderRes = await orderClient.AddOrderAsync(req);
+    Console.WriteLine($"Success:{addOrderRes.Success}");
+    Console.WriteLine($"OrderId:{addOrderRes.OrderId}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+Console.WriteLine("Hello, World!");
+Console.Read();
---
+++ Wmj.Hosting.Grpc/Services/OrderServices.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Core;
+using Wmj.Hosting.Grpc.Protos;
+
+namespace Wmj.Hosting.Grpc.Services
+{
+    public class OrderServices:Order.OrderBase
+    {
+        public override Task<AddOrderRes> AddOrder(AddOrderReq request, ServerCallContext context)
+        {
+            string name=request.Name;
+            Console.WriteLine($"Received request from client, Name is {name}");
+            Console.WriteLine(name);
+            AddOrderRes res=new AddOrderRes();
+            res.OrderId = 111;
+            res.Success = true;
+            return Task.FromResult(res);
+            //return base.AddOrder(request, context);
+        }
+    }
+}
